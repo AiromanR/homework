33862f194e9d8aa40ba07c1cf67640be0c4c3a5c
AiromanR <110393090+AiromanR@users.noreply.github.com>
2025-04-11 10:45:58 +0500

    Add files via upload

---
+++ prKol_ind2_Rogov/ProgramV9.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+
+        string filePath = "students.txt";
+        if (File.Exists(filePath))
+        {
+            Queue<string> successfulStudents = new Queue<string>();
+            Queue<string> otherStudents = new Queue<string>();
+
+
+            foreach (string line in File.ReadLines(filePath))
+            {
+                string[] parts = line.Split(new[] { " " }, StringSplitOptions.None);
+                // первые 4 это фио и группа, их пропускаем
+                int[] grades = parts.Skip(4).Select(int.Parse).ToArray();
+
+                if (grades.All(grade => grade >= 4))
+                    successfulStudents.Enqueue(line);
+                else
+                    otherStudents.Enqueue(line);
+            }
+
+            Console.WriteLine("Студенты, успешно сдавшие сессию:");
+            while (successfulStudents.Count > 0)
+                Console.WriteLine(successfulStudents.Dequeue());
+
+            Console.WriteLine("\nОстальные студенты:");
+            while (otherStudents.Count > 0)
+                Console.WriteLine(otherStudents.Dequeue());
+        }
+        else { 
+            Console.WriteLine("Файл не обнаружен");
+        }
+    }
+}
